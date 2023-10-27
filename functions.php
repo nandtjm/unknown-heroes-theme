39dd7a012154ccda3bca97b0cbc86ce40ea091b8
nandtjm <codingwithnand1@gmail.com>
2023-10-27 12:46:23 +0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'UNKWON_HEROS_VERSION', '2.0.0' );
+
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
+function unkwon_heros_scripts_styles() {
+
+	wp_enqueue_style(
+		'unkwon-heros-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		UNKWON_HEROS_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'unkwon_heros_scripts_styles', 20 );
