c51ec144e0ea2e3495af5feb2e9e41c94b4cbcc7
nandtjm <codingwithnand1@gmail.com>
2023-11-09 14:21:49 +0500

    auto select variation

--- js/ukh-custom-script.js
+++ js/ukh-custom-script.js
@@ -20,5 +20,29 @@
 			$('.single main').css('margin-top', marginTop + 'px');
 		}
 
+		$( document.body ).on( 'woocommerce_variation_has_changed', function(e) {
+			if ( $('.summary .single_add_to_cart_button.disabled').length > 0 &&
+				$('.summary .variable-item.no-stock').length > 0 ) {
+				$('.summary .single_add_to_cart_button').text('Nicht vorrätig');
+			} else {
+				$('.summary .single_add_to_cart_button').text('In den Warenkorb');
+			}
+		});
+
+		if ( $('.summary .variable-item.selected').length === 0 ) {
+			setTimeout(function(){
+				$('.summary .variable-item:not(.no-stock):first').trigger('click');
+				
+				$('.summary .reset_variations').hide();
+			}, 300);
+		}
+		$(".woof_container_inner_produkt-kategorien h4").text("PRODUKTE");
+		$(".woof_container_inner_produktbrand h4").text("KOLLEKTIONEN");
+		$(".woof_container_inner_produktgeschlecht h4").text("GESCHLECHT");
+
+		$(".open-cookie-bot").click(function(){
+			$(".CookiebotWidget-logo").trigger("click");
+		});
+
 	});
 })(jQuery);
