fd118c50ff0fe3feedb84c00050af6ed2ca20fd6
nandtjm <codingwithnand1@gmail.com>
2023-12-21 00:52:02 +0500

    modify query for brand

--- functions.php
+++ functions.php
@@ -132,7 +132,7 @@ add_action('init', 'add_custom_endpoint');
 
 function custom_brand_endpoint_redirect() {
     global $wp_query;
-    var_dump($wp_query->query_vars['brand']);
+    var_dump($_GET['brand']);
     if (isset($wp_query->query_vars['brand'])) {
         $brand_slug = sanitize_text_field($wp_query->query_vars['brand']);
         $tax_query = array(
