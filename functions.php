718dd8f8e7b0bc6392c6ee53fd08c97583397450
nandtjm <codingwithnand1@gmail.com>
2023-12-21 10:32:08 +0500

    debugging off

--- functions.php
+++ functions.php
@@ -171,7 +171,7 @@ function custom_brand_endpoint_query($query) {
 }
 add_action('pre_get_posts', 'custom_brand_endpoint_query');
 
-// global $wp_rewrite;
+//global $wp_rewrite;
 // echo '<pre>';
 // print_r($wp_rewrite->wp_rewrite_rules());
 
