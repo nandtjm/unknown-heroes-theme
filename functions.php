31e343d088637d9e405492856d96aad33a32d03a
nandtjm <codingwithnand1@gmail.com>
2024-02-03 01:10:44 +0500

    fix size attribute ordering

--- functions.php
+++ functions.php
@@ -393,7 +393,7 @@ function custom_product_attribute_sorting( $terms, $taxonomies, $args ) {
     if ( in_array( 'pa_size', $taxonomies ) ) {
         // Define the desired order of size attributes
         $desired_order = array( 'XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL' );
-        var_dump($terms);
+        echo '<pre>'; print_r($terms); echo '</pre>';
         // Sort the terms based on the desired order
         usort( $terms, function ( $a, $b ) use ( $desired_order ) {
             $a_index = array_search( $a, $desired_order );
