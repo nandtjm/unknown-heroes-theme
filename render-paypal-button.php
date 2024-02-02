b4c36be1366e25e420fdedf87058524fe2ca2946
nandtjm <codingwithnand1@gmail.com>
2024-02-02 09:56:21 +0500

    added paypal button shortcode

--- render-paypal-button.php
+++ render-paypal-button.php
@@ -3,6 +3,7 @@ defined('ABSPATH') || exit;
 
 $services_path = WP_PLUGIN_DIR . '/woocommerce-paypal-payments/modules/ppcp-button/module.php';
 if ( file_exists( $services_path ) ) {
-    require_once $services_path;
+    var_dump($services_path);
+    include $services_path;
 }
 
